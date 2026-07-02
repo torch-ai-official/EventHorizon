1460a02c4df82c6d62607ca29194da222d394f98
torch-ai-official <lucasthd08@gmail.com>
2026-07-01 22:24:55 -0300

    Excluir 2026.py

--- 2026.py
+++
@@ -1,7 +0,0 @@
-# Cria uma string única concatenando todos os números de 0 a 7000
-sequencia_gigante = "".join(str(n) for n in range(700001))
-
-# Conta as ocorrências de "2026" na string completa
-total = sequencia_gigante.count("2026")
-
-print(f"A sequência '2026' aparece {total} vezes.")
